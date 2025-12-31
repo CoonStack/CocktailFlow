f09c551cd8dd2696682764572dd893027fd025b0
Kay <lunar1323@gmail.com>
2025-12-31 15:04:46 +0900

    Fix router basename for GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { Favorites } from './pages/Favorites';
 
 function App() {
   return (
-    <Router>
+    <Router basename={import.meta.env.BASE_URL}>
       <Layout>
         <Routes>
           <Route path="/" element={<Home />} />
